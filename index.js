e8bb95c51e8f03028bbc581791af8e84f89d4907
Azat Akmyradov <im.akmyradov@gmail.com>
2023-04-25 00:22:55 -0400

    changed the structure of the project and moved Storage classes to "src" folder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const storage = require('./Storage.js');
-const GoogleCloudStorage = require('./GoogleCloudStorage.js');
-const AmazonS3Storage = require('./AmazonS3Storage.js');
+const storage = require('./src/Storage.js');
+const GoogleCloudStorage = require('./src/GoogleCloudStorage.js');
+const AmazonS3Storage = require('./src/AmazonS3Storage.js');
 
 module.exports = {
     Storage: storage,
